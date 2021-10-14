ee0ca0bec4883e3e1873a939bcc1105bf99a4809
马超 <machao@seiue.com>
2021-10-14 20:43:37 +0800

    packages/tour export ContentProps type

--- packages/tour/src/index.tsx
+++ packages/tour/src/index.tsx
@@ -3,4 +3,4 @@ import TourContext, { TourProvider, useTour } from './Context'
 export { default as withTour } from './withTour'
 export default Tour
 export { Tour, TourContext, TourProvider, useTour }
-export { StepType, Position } from './types'
+export { StepType, Position, ContentProps } from './types'
